53bf0d040a09b0015e0af50268414c38130b6bed
Roberto Valdetaro <rvaldetaro@gmail.com>
2023-11-10 10:49:36 -0300

    curso javascript

---
+++ type-boolean.js
@@ -0,0 +1,29 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero);
+// Resultado = false
+
+
+const primeiroNumero1 = 5;
+const segundoNumero2 = 5;
+const cadastroAtivado1 = true;
+
+// console.log(primeiroNumero1 === segundoNumero2);
+// Resultado = true
+
+const texto0 = "Alura";
+const texto1 = "a";
+
+console.log(texto0 === texto1)
+
+const texto2 = "Alura";
+const texto3 = "alura";
+
+console.log(texto2 === texto3)
---
+++ type-number.js
@@ -0,0 +1,6 @@
+const number1 = 1;
+const number2 = .5;
+
+const resultado = number1 / number2;
+
+console.log(resultado);
