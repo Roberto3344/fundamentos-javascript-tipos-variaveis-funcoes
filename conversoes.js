38093be8282aa1c20f9fed7365dc5b9ad42a579e
Roberto Valdetaro <rvaldetaro@gmail.com>
2023-11-13 17:54:14 -0300

    Atualizando

--- conversoes.js
+++ conversoes.js
@@ -2,3 +2,16 @@ const numero = 456;
 const numeroString = "456"; 
 
 console.log(numero + numeroString)
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
